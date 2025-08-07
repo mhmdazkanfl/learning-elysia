d64647717f4b5aa10ec394dcd87d53f69e8dcee1
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-08-07 20:54:15 +0700

    feat: add user and note services with authentication and CRUD operations
    - Introduced user service for sign-up, sign-in, and profile retrieval.
    - Implemented note service for creating, updating, retrieving, and deleting notes.
    - Integrated OpenTelemetry and Swagger for monitoring and API documentation.
    - Enhanced error handling and logging for better debugging.
    - Added Prettier for code formatting.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+import { opentelemetry } from '@elysiajs/opentelemetry'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { note } from './note'
+import { user } from './user'
+
+const app = new Elysia()
+    .use(opentelemetry())
+    .use(swagger())
+    .onError(({ error, code }) => {
+        if (code === 'NOT_FOUND') return
+
+        console.error(error)
+    })
+    .use(user)
+    .use(note)
+    .listen(3000)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+)
